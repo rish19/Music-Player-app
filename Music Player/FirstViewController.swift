9384875cc7dbf0a2eb66c676a0144f81fb304fea
Rishabh Bhandari <rishabh.bhandari53@gmail.com>
2019-06-11 11:56:37 +0530

    Initial Commit

---
+++ Music Player/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Music Player
+//
+//  Created by Rishabh Bhandari on 11/06/19.
+//  Copyright © 2019 Rishabh Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Music Player/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Music Player
+//
+//  Created by Rishabh Bhandari on 11/06/19.
+//  Copyright © 2019 Rishabh Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
